58c2974df28b54a09dadcfac0971bf0d94463b72
Thing Thinge <thing@something.com>
2021-02-11 20:49:50 +0100

    First

---
+++ app/src/main/java/com/example/aurora/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.aurora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import org.json.*;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void
+}
